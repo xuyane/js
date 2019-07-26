d204b447c9493053725df3f2e59ae462ae809695
xiaobai <419456942@qq.com>
2019-07-26 09:11:58 +0800

    js面向对象

--- js/src/index.js
+++ js/src/index.js
@@ -1,9 +1,33 @@
-class People {
-  constructor(name){
-    this.name = name
+class jQuery {
+  constructor(seletor) {
+    let slice = Array.prototype.slice;
+    console.log('slice', slice);
+    let dom = slice.call(document.querySelectorAll(seletor));
+    console.log('dom', dom);
+    let len = dom ? dom.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.seletor = seletor || ''
+    console.log(this.length,this.seletor)
   }
-  saySomething(){
+  append(node) {
 
   }
+  addClass(name) {
+
+  }
+  html(data) {
+
+  }
+  // 此处省略N个API
 }
-class
+
+window.$ = function (seletor) {
+  return new jQuery(seletor)
+}
+
+var $p = $('p')
+console.log($p)
+console.log($p.addClass)
